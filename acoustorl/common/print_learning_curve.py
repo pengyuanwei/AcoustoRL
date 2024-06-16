1651f978b551672d60d2dddb6c4b28b0474f0ac6
Pengyuan Wei <53529144+pengyuanwei@users.noreply.github.com>
2024-06-16 01:05:41 +0100

    Add files via upload

--- acoustorl/common/print_learning_curve.py
+++ acoustorl/common/print_learning_curve.py
@@ -16,18 +16,18 @@ if __name__ == "__main__":
     #minimal_buffer_size = 100000
     #eval_interval = 5000
     num_experiment = 5
-    env_name = "Hopper-v3"
+    env_name = 'Walker2d-v4'  #'HalfCheetah-v2', 'Hopper-v3', 'Walker2d-v4'
 
     algorithm = "TD3"
     target_folder = "../../../test_results/%s_%s"%(env_name, algorithm)
 
     # 读取
-    total_return_array = np.zeros([num_experiment,90])
+    total_return_array = np.zeros([num_experiment,100])
     for i in range(num_experiment):
         file_name = f"return_list{i}.npy"
         target_file = os.path.join(target_folder, file_name)
         return_list = np.load(target_file)
-        return_list = return_list[-90:]
+        #return_list = return_list[-90:]
         total_return_array[i] = return_list
 
     # Calculate the mean
@@ -44,16 +44,42 @@ if __name__ == "__main__":
 
 
     ####################################################################################################
-    algorithm = "TD3wPER"
+    algorithm = "TD3_per_MSE"
     target_folder = "../../../test_results/%s_%s"%(env_name, algorithm)
 
     # 读取
-    total_return_array = np.zeros([num_experiment,90])
+    total_return_array = np.zeros([num_experiment,100])
     for i in range(num_experiment):
         file_name = f"return_list{i}.npy"
         target_file = os.path.join(target_folder, file_name)
         return_list = np.load(target_file)
-        return_list = return_list[-90:]
+        #return_list = return_list[-90:]
+        total_return_array[i] = return_list
+
+    # Calculate the mean
+    avg_return = np.mean(total_return_array, axis=0)
+
+    # 计算第 25 和第 75 百分位数 (用于阴影区域)
+    Q1 = np.percentile(total_return_array, 25, axis=0)
+    Q3 = np.percentile(total_return_array, 75, axis=0)
+
+    # 绘制阴影区域
+    plt.fill_between(range(avg_return.shape[0]), Q1, Q3, color=(0.0, 1.0, 0.0, 0.1), alpha=0.2)
+    # 绘制平均值曲线
+    plt.plot(avg_return, label=algorithm, color=(0.0, 1.0, 0.0, 1.0), linewidth=2)
+
+
+    ####################################################################################################
+    algorithm = "TD3_per_Huber"
+    target_folder = "../../../test_results/%s_%s"%(env_name, algorithm)
+
+    # 读取
+    total_return_array = np.zeros([num_experiment,100])
+    for i in range(num_experiment):
+        file_name = f"return_list{i}.npy"
+        target_file = os.path.join(target_folder, file_name)
+        return_list = np.load(target_file)
+        #return_list = return_list[-90:]
         total_return_array[i] = return_list
 
     # Calculate the mean
@@ -70,7 +96,7 @@ if __name__ == "__main__":
 
 
     # 添加标签和图例
-    plt.xlabel('Timesteps')
+    plt.xlabel('Timesteps (1e4)')
     plt.ylabel('Returns')
     plt.title(env_name)
     plt.legend(loc='lower right')

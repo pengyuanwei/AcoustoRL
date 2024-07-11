682350be272b9c890dfe2130241f95d5c35b3e50
Pengyuan Wei <53529144+pengyuanwei@users.noreply.github.com>
2024-07-11 18:03:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/AcoustoRL",
     packages=find_packages(),
-    install_requires=["gymnasium==0.29.1",
-                      "tqdm==4.66.4",],    
+    install_requires=[],    
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",

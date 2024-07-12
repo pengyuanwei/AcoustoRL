3853e67c74c424e67c56539fd4617c17be9c4288
Pengyuan Wei <53529144+pengyuanwei@users.noreply.github.com>
2024-07-12 20:23:50 +0100

    Add files via upload

--- acoustorl/DDPG.py
+++ acoustorl/DDPG.py
@@ -1,9 +1,8 @@
+import os
 import copy
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import os
 
 
 # Re-tuned version of Deep Deterministic Policy Gradients (DDPG)

9719dffbe3908e4ca32809783887362ef22c2ae3
Pengyuan Wei <53529144+pengyuanwei@users.noreply.github.com>
2024-07-13 00:21:36 +0100

    Add files via upload

---
+++ acoustorl/MADDPG.py
@@ -0,0 +1,118 @@
+import torch
+import torch.nn.functional as F
+
+
+class TwoLayerFC(torch.nn.Module):
+    def __init__(self, num_in, num_out, hidden_dim):
+        super().__init__()
+        self.fc1 = torch.nn.Linear(num_in, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = torch.nn.Linear(hidden_dim, num_out)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+
+
+class DDPG:
+    ''' DDPG算法 '''
+    def __init__(self, state_dim, action_dim, critic_input_dim, hidden_dim,
+                 actor_lr, critic_lr, device):
+        self.actor = TwoLayerFC(state_dim, action_dim, hidden_dim).to(device)
+        self.target_actor = TwoLayerFC(state_dim, action_dim,
+                                       hidden_dim).to(device)
+        self.critic = TwoLayerFC(critic_input_dim, 1, hidden_dim).to(device)
+        self.target_critic = TwoLayerFC(critic_input_dim, 1,
+                                        hidden_dim).to(device)
+        self.target_critic.load_state_dict(self.critic.state_dict())
+        self.target_actor.load_state_dict(self.actor.state_dict())
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(),
+                                                lr=actor_lr)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(),
+                                                 lr=critic_lr)
+
+    def take_action(self, state, explore=False):
+        action = self.actor(state)
+        if explore:
+            action = gumbel_softmax(action)
+        else:
+            action = onehot_from_logits(action)
+        return action.detach().cpu().numpy()[0]
+
+    def soft_update(self, net, target_net, tau):
+        for param_target, param in zip(target_net.parameters(),
+                                       net.parameters()):
+            param_target.data.copy_(param_target.data * (1.0 - tau) +
+                                    param.data * tau)
+
+class MADDPG:
+    def __init__(self, env, device, actor_lr, critic_lr, hidden_dim,
+                 state_dims, action_dims, critic_input_dim, gamma, tau):
+        self.agents = []
+        for i in range(len(env.agents)):
+            self.agents.append(
+                DDPG(state_dims[i], action_dims[i], critic_input_dim,
+                     hidden_dim, actor_lr, critic_lr, device))
+        self.gamma = gamma
+        self.tau = tau
+        self.critic_criterion = torch.nn.MSELoss()
+        self.device = device
+
+    @property
+    def policies(self):
+        return [agt.actor for agt in self.agents]
+
+    @property
+    def target_policies(self):
+        return [agt.target_actor for agt in self.agents]
+
+    def take_action(self, states, explore):
+        states = [
+            torch.tensor([states[i]], dtype=torch.float, device=self.device)
+            for i in range(len(env.agents))
+        ]
+        return [
+            agent.take_action(state, explore)
+            for agent, state in zip(self.agents, states)
+        ]
+
+    def update(self, sample, i_agent):
+        obs, act, rew, next_obs, done = sample
+        cur_agent = self.agents[i_agent]
+
+        cur_agent.critic_optimizer.zero_grad()
+        all_target_act = [
+            onehot_from_logits(pi(_next_obs))
+            for pi, _next_obs in zip(self.target_policies, next_obs)
+        ]
+        target_critic_input = torch.cat((*next_obs, *all_target_act), dim=1)
+        target_critic_value = rew[i_agent].view(
+            -1, 1) + self.gamma * cur_agent.target_critic(
+                target_critic_input) * (1 - done[i_agent].view(-1, 1))
+        critic_input = torch.cat((*obs, *act), dim=1)
+        critic_value = cur_agent.critic(critic_input)
+        critic_loss = self.critic_criterion(critic_value,
+                                            target_critic_value.detach())
+        critic_loss.backward()
+        cur_agent.critic_optimizer.step()
+
+        cur_agent.actor_optimizer.zero_grad()
+        cur_actor_out = cur_agent.actor(obs[i_agent])
+        cur_act_vf_in = gumbel_softmax(cur_actor_out)
+        all_actor_acs = []
+        for i, (pi, _obs) in enumerate(zip(self.policies, obs)):
+            if i == i_agent:
+                all_actor_acs.append(cur_act_vf_in)
+            else:
+                all_actor_acs.append(onehot_from_logits(pi(_obs)))
+        vf_in = torch.cat((*obs, *all_actor_acs), dim=1)
+        actor_loss = -cur_agent.critic(vf_in).mean()
+        actor_loss += (cur_actor_out**2).mean() * 1e-3
+        actor_loss.backward()
+        cur_agent.actor_optimizer.step()
+
+    def update_all_targets(self):
+        for agt in self.agents:
+            agt.soft_update(agt.actor, agt.target_actor, self.tau)
+            agt.soft_update(agt.critic, agt.target_critic, self.tau)
